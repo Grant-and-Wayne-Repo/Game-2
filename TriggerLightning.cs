a67b362621af5d20e24dddda91ff1b5bbbd739c6
Grant Collins <grantcol@usc.edu>
2013-05-06 13:53:59 -0700

    Fixed collider rotation!

--- TriggerLightning.cs
+++ TriggerLightning.cs
@@ -6,7 +6,7 @@ public class TriggerLightning : MonoBehaviour {
 	private LineRenderer LR;
 	private Transform [] lPads;
 	public int count;
-	// Use this for initialization
+
 	void Start () {
 		lPads = new Transform[20];
 		LR = new LineRenderer();
@@ -15,25 +15,34 @@ public class TriggerLightning : MonoBehaviour {
 		count = 0;
 	}
 	
-	// Update is called once per frame
 	void OnTriggerEnter (Collider other) {
 		if(other.gameObject.tag == "LPad")
 		{
 			count++;
 			Debug.Log ("Hit");
 			lPads[count] = other.gameObject.transform;
-			if(lPads.Length > 1)
+			
+			if(count == 2)
 			{
-				Vector3 mid = (lPads[count].position - lPads[count - 1].position)/2;
-				var lclone = Instantiate(LPad, mid/*((lPads[1].position.y - lPads[0].position.y)/2)other.gameObject.transform.position*/, Quaternion.identity) as GameObject;
+				Vector3 mid = (lPads[count -1].position - lPads[count].position)/2 + lPads[count].position;
+				var lclone = Instantiate(LPad, mid, Quaternion.identity) as GameObject;
 				
+				float dy = lclone.transform.position.y - lPads[count].position.y;
+				float dx = lclone.transform.position.x - lPads[count].position.x;
+        		float angle = Mathf.Atan2(dy,dx); 
+				float rotZ = angle * (180/Mathf.PI) - 90;
+				lclone.transform.eulerAngles = new Vector3(lclone.transform.rotation.x, lclone.transform.rotation.y, rotZ);
 				lclone.transform.localScale += new Vector3(0, Vector3.Distance(lPads[count].position, lPads[count - 1].position), 5);
 				
 				LR.SetWidth(5, 5);
 				LR.SetVertexCount(count);
-				LR.SetPosition(0, lPads[count-1].position);
-				LR.SetPosition(1, lPads[count].position);
+				LR.SetPosition(0, lPads[count].position);
+				LR.SetPosition(1, lPads[count-1].position);
+				
+				
+				count = 0;
 			}
+			
 		}
 		else if(other.gameObject.tag == "XPad")
 		{
