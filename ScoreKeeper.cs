fc095629ade187fa4dc83d67d34fa267c0dac505
Grant Collins <grantcol@usc.edu>
2013-05-06 18:38:41 -0700

    ScoreKeeper script close#4
    We can add our final gui onto this script so it is organized

---
+++ ScoreKeeper.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class ScoreKeeper : MonoBehaviour {
+	
+	public Rect scoreLabelRect = new Rect(10,10, 200, 50);
+	private int Score;
+	
+	void Start () {
+		Score = 0;
+	}
+	
+	void Update () {
+		int sum = TriggerLightning.score + Explode.escore + Zap.zscore;
+		Score = sum;
+	}		
+	void OnGUI()
+	{
+		GUI.Label (scoreLabelRect, "Score "+ Score.ToString());
+	}
+}
