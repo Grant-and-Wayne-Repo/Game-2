0d2875560b02471d082bdb2500076521cf8f6ef2
Grant Collins <grantcol@usc.edu>
2013-05-08 11:37:22 -0700

    Updated ScoreKeeper

--- ScoreKeeper.cs
+++ ScoreKeeper.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class ScoreKeeper : MonoBehaviour {
 	
 	public Rect scoreLabelRect = new Rect(10,10, 200, 50);
+	public Rect timeLabelRect = new Rect(350, 10, 200, 100);
 	static public int Score;
+	
 	
 	void Start () {
 		Score = 0;
@@ -17,5 +19,19 @@ public class ScoreKeeper : MonoBehaviour {
 	void OnGUI()
 	{
 		GUI.Label (scoreLabelRect, "Score "+ Score.ToString());
+		string ft = formatedTimeString(Time.time);
+		GUI.Label (timeLabelRect,  ft);
+	}
+	private string formatedTimeString (float input) 
+	{
+   	 	int seconds;
+    	int minutes;
+ 
+    	minutes = Mathf.FloorToInt(input / 60);
+    	seconds = Mathf.FloorToInt(input - minutes * 60);
+    	string r = (minutes < 10) ? "0" + minutes.ToString() : minutes.ToString();
+    	r += ":";
+    	r += (seconds < 10) ? "0" + seconds.ToString() : seconds.ToString();
+    	return r;
 	}
 }
